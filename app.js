ca46d9c49ec90c57b994509f2e1a4f9cc8a2306e
parasdeep <parasdeepoo78@gmail.com>
2023-05-02 21:35:46 +0530

    new soni

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.static("css"));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+
+        "/index.html");
+});
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function sayHello(){
+    var name=
+    document.getElementById("name").value;
+    var message="<h2> hello "+name+"!</h2>";
+    document.getElementById("content").innerHTML=message;
+}
